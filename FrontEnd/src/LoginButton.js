c55ec0b3a820463b5cc5b9e7fd6ed03492a19cce
Alex <cal368@cornell.edu>
2020-02-20 22:56:09 -0500

    Button now appropriately switches pages

--- FrontEnd/src/LoginButton.js
+++ FrontEnd/src/LoginButton.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 
 class LoginButton extends Component {
 
     render(){
       return (
+          <Link to="/assignment">
           <button className="log_but"> Cornell Login</button>
+          </Link>
       )
     }
   }
