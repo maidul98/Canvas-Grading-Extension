71655dd97ce17e983f669aab49b8b4843bb80393
Alex <cal368@cornell.edu>
2020-02-20 21:13:57 -0500

    Individual Dashboard initial file setup

---
+++ FrontEnd/src/dashboard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './dashboard.css';
+import * as serviceWorker from './serviceWorker';
+
+
+
+
+
+class Screen extends React.Component {
+
+  render(){
+    return (
+      <div>
+      <div class="top_bar">
+      Canvas Grading Extension
+      </div>
+      <div class = "mid">
+      Name of Student:
+      </div>
+      </div>
+    )
+  }
+
+}
+
+
+
+ReactDOM.render(<Screen/>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
