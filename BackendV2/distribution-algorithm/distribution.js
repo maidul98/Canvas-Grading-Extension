96c2baa34f4710190415bc4226ff276fda105587
Ansh Godha <anshgodha714@gmail.com>
2020-03-12 11:24:58 -0400

    initial tests for dist

--- BackendV2/distribution-algorithm/distribution.js
+++ BackendV2/distribution-algorithm/distribution.js
@@ -14,7 +14,7 @@ function distribute(num_of_submissions, graders) {
   total_weight = graders.reduce(function (total, element) {
     return total + element[1];
   }, 0);
-
+  console.log('Total weight: ' + total_weight)
 
   //shuffle grader 2D-array 
   shuffle(graders);
@@ -25,6 +25,7 @@ function distribute(num_of_submissions, graders) {
   let graderArray = []
   graders.forEach(element => {
     assigned = Math.floor(element[1] * num_of_submissions / total_weight);
+    console.log(`Number assigned to id ${element[0]}: ${assigned}`)
     //grader_id, weight, offset, num_assigned
     graderArray.push(AssignmentGrader(element[0], element[1], element[2], assigned));
   });
@@ -35,12 +36,12 @@ function distribute(num_of_submissions, graders) {
   currDist = graderArray.reduce(function (total, element) {
     return total + element.num_assigned;
   }, 0);
-
+  console.log('CurrentDist: ' + currDist)
 
 
   //calculates difference between (num_of_submissions) and (currDist)
-
-
+  remainingAssignments = num_of_submissions - currDist;
+  console.log('remaining assignments: ' + remainingAssignments)
 
   //sort graders in order of worst to best offsets 
 
@@ -64,3 +65,12 @@ function distribute(num_of_submissions, graders) {
 let testArr = [[1, 3], [2, 4], [4, 5], [4, 5]];
 distribute(0, 0, testArr);
 
+//Create dummy graders
+let g1 = [1223, 0.5, 0]
+let g2 = [1224, 0.2, -1]
+let g3 = [1225, 0.3, 2]
+let g4 = [1226, 0.3, -1]
+let g5 = [1227, 0.3, -1]
+
+let graders = [g1, g2, g3, g4, g5]
+distribute(50, graders)
