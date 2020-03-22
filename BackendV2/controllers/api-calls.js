2a807c39af019792f7f4264998f5290cbafd4b80
Alex Lee <cal368@cornell.edu>
2020-03-22 14:53:20 -0400

    ESLint Basic style fixes in Backend

--- BackendV2/controllers/api-calls.js
+++ BackendV2/controllers/api-calls.js
@@ -2,138 +2,138 @@
  * This file will store all the logic for retrieving and sending data using the 
  * Canvas API. All database querying logic should be put in queries.js.
  */
-const axios = require('axios')
+const axios = require('axios');
 const queries = require('./queries');
-const qs = require('qs')
-const http = require('http')
-const fs = require('fs')
+const qs = require('qs');
+const http = require('http');
+const fs = require('fs');
 
 const config = {
-  //TODO: Factor out bearer tokens into another file that isn't publicly accessible.
-  headers: { Authorization: `Bearer 9713~TYz9t4zPXdeHonsL9g19ac3kIucoU8BdskLUNZ1rijvusRvhhdbyQFMhXPDhDltZ` }
+    //TODO: Factor out bearer tokens into another file that isn't publicly accessible.
+    headers: { Authorization: 'Bearer 9713~TYz9t4zPXdeHonsL9g19ac3kIucoU8BdskLUNZ1rijvusRvhhdbyQFMhXPDhDltZ' }
 };
 
 /** Obtains all the student enrollments for the specific class. */
 exports.student_enrollments = function (_, res) {
-  try {
-    axios.get("https://canvas.cornell.edu/api/v1/courses/15037/enrollments",
-      config)
-      .then(response => {
-        result = []
-        response.data.forEach(function (element) {
-          if (element.type == "StudentEnrollment") {
-            result.push(element);
-          };
-        });
-        res.json(result);
-        return result
-      })
-      .catch(err => res.send(err));
-  }
-  catch (err) {
-    console.error("GG", err);
-  }
-}
+    try {
+        axios.get('https://canvas.cornell.edu/api/v1/courses/15037/enrollments',
+            config)
+            .then(response => {
+                result = [];
+                response.data.forEach(function (element) {
+                    if (element.type == 'StudentEnrollment') {
+                        result.push(element);
+                    }
+                });
+                res.json(result);
+                return result;
+            })
+            .catch(err => res.send(err));
+    }
+    catch (err) {
+        console.error('GG', err);
+    }
+};
 
 
 exports.download_single_submission = function (req, res) {
-  console.log('method called')
-  axios
-    .get(`https://canvas.cornell.edu/api/v1/courses/15037/${req.params.assignment_id}/submissions/${req.params.user_id}`, config)
-    .then(result => {
-      const attachments = result.attachments
-      attachments.forEach(attachment => {
-        console.log(attachment.url)
-        res.download(attachment.url)
-      })
-    })
-    .catch(error => console.log(error));
-}
+    console.log('method called');
+    axios
+        .get(`https://canvas.cornell.edu/api/v1/courses/15037/${req.params.assignment_id}/submissions/${req.params.user_id}`, config)
+        .then(result => {
+            const attachments = result.attachments;
+            attachments.forEach(attachment => {
+                console.log(attachment.url);
+                res.download(attachment.url);
+            });
+        })
+        .catch(error => console.log(error));
+};
 
 exports.get_published_assignments = function (_, res) {
-  axios
-    .get('https://canvas.cornell.edu/api/v1/courses/15037/assignments', config)
-    .then(result => {
-      const assignmentJSONArray = result.data;
-      const filtered = assignmentJSONArray.filter(assignment => assignment.published);
-      // queries.insertPublishedAssignments(filtered);
-      return filtered
-    }).then(json => {
-      res.json(json);
-    })
-    .catch(error => console.log(error));
-}
+    axios
+        .get('https://canvas.cornell.edu/api/v1/courses/15037/assignments', config)
+        .then(result => {
+            const assignmentJSONArray = result.data;
+            const filtered = assignmentJSONArray.filter(assignment => assignment.published);
+            // queries.insertPublishedAssignments(filtered);
+            return filtered;
+        }).then(json => {
+            res.json(json);
+        })
+        .catch(error => console.log(error));
+};
 
 exports.get_submissions_for_assignment = function (req, res) {
-  // console.log('hey')
-  // res.status(400);
-  // res.send('None shall pass');
-  axios
-    .get(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${req.params.assignment_id}/submissions`, config)
-    .then(result => {
-      const submissionsJSONArray = result.data;
-      res.json(submissionsJSONArray);
-    })
-    .catch(error => console.log(error));
-}
+    // console.log('hey')
+    // res.status(400);
+    // res.send('None shall pass');
+    axios
+        .get(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${req.params.assignment_id}/submissions`, config)
+        .then(result => {
+            const submissionsJSONArray = result.data;
+            res.json(submissionsJSONArray);
+        })
+        .catch(error => console.log(error));
+};
 
 exports.get_assignments_table = function (req, res) {
-  axios
-    .get(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${req.params.assignment_id}/submissions`, config)
-    .then(result => {
-      const submissionsJSONArray = result.data;
-      res.json(submissionsJSONArray);
-    })
-    .catch(error => console.log(error));
-}
+    axios
+        .get(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${req.params.assignment_id}/submissions`, config)
+        .then(result => {
+            const submissionsJSONArray = result.data;
+            res.json(submissionsJSONArray);
+        })
+        .catch(error => console.log(error));
+};
 
 exports.get_all_graders = function (_, res) {
-  axios
-    .get("https://canvas.cornell.edu/api/v1/courses/15037/enrollments",
-      config)
-    .then(response => {
-      result = []
-      response.data.forEach(function (element) {
-        //TaEnrollment - TAs
-        //DesignerEnrollment - Consultants
-        //ObserverEnrollment - Graders
-        if (element.type === "TaEnrollment" || element.type === "DesignerEnrollment" || element.type === "ObserverEnrollment") {
-          result.push(element);
-        };
-      });
-      return result
-    })
-    .then(jsonarr => {
-      queries.insertAllGraders(jsonarr);
-      return jsonarr
-    })
-    .then(resj => {
-      res.json(resj)
-    })
-    .catch(err => res.send(err));
-}
+    axios
+        .get('https://canvas.cornell.edu/api/v1/courses/15037/enrollments',
+            config)
+        .then(response => {
+            result = [];
+            response.data.forEach(function (element) {
+                //TaEnrollment - TAs
+                //DesignerEnrollment - Consultants
+                //ObserverEnrollment - Graders
+                if (element.type === 'TaEnrollment' || element.type === 'DesignerEnrollment' || element.type === 'ObserverEnrollment') {
+                    result.push(element);
+                }
+            });
+            return result;
+        })
+        .then(jsonarr => {
+            queries.insertAllGraders(jsonarr);
+            return jsonarr;
+        })
+        .then(resj => {
+            res.json(resj);
+        })
+        .catch(err => res.send(err));
+};
 
 exports.grade_single_submission = function (req, res) {
 
-  let formData = {
-    'comment[text_comment]': req.body.comment,
-    'comment[group_comment]': req.body.is_group_comment,
-    'submission[posted_grade]': req.body.assigned_grade
-  }
+    let formData = {
+        'comment[text_comment]': req.body.comment,
+        'comment[group_comment]': req.body.is_group_comment,
+        'submission[posted_grade]': req.body.assigned_grade
+    };
 
-  let headerData = {
-    headers: {
-      'Accept': 'application/json',
-      "Authorization": `Bearer 9713~TYz9t4zPXdeHonsL9g19ac3kIucoU8BdskLUNZ1rijvusRvhhdbyQFMhXPDhDltZ`
-    }
-  }
-  axios
-    .put(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${req.params.assignment_id}/submissions/${req.params.user_id}`, qs.stringify(formData), headerData)
-    .then(r => {
-      res.send('done');
-    })
-    .catch(err => console.log(err));
-}
+    let headerData = {
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer 9713~TYz9t4zPXdeHonsL9g19ac3kIucoU8BdskLUNZ1rijvusRvhhdbyQFMhXPDhDltZ'
+        }
+    };
+    axios
+        .put(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${req.params.assignment_id}/submissions/${req.params.user_id}`, qs.stringify(formData), headerData)
+        .then(r => {
+            res.send('done');
+        })
+        .catch(err => console.log(err));
+};
 
 /** Sample json in req.body:
  *[
@@ -152,24 +152,24 @@ exports.grade_single_submission = function (req, res) {
   ]
  */
 exports.grade_batch_submissions = function (req, res) {
-  let formData = {}
-  req.body.forEach(j => {
-    formData[`grade_data[${j.id}][text_comment]`] = j.comment;
-    formData[`grade_data[${j.id}][group_comment]`] = j.is_group_comment;
-    formData[`grade_data[${j.id}][posted_grade]`] = j.assigned_grade;
-  });
-  console.log(formData)
-  let headerData = {
-    headers: {
-      'Accept': 'application/json',
-      "Authorization": `Bearer 9713~TYz9t4zPXdeHonsL9g19ac3kIucoU8BdskLUNZ1rijvusRvhhdbyQFMhXPDhDltZ`
-    }
-  }
+    let formData = {};
+    req.body.forEach(j => {
+        formData[`grade_data[${j.id}][text_comment]`] = j.comment;
+        formData[`grade_data[${j.id}][group_comment]`] = j.is_group_comment;
+        formData[`grade_data[${j.id}][posted_grade]`] = j.assigned_grade;
+    });
+    console.log(formData);
+    let headerData = {
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer 9713~TYz9t4zPXdeHonsL9g19ac3kIucoU8BdskLUNZ1rijvusRvhhdbyQFMhXPDhDltZ'
+        }
+    };
 
-  axios
-    .post(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${req.params.assignment_id}/submissions/update_grades`, qs.stringify(formData), headerData)
-    .then(result => {
-      res.send('success')
-    })
-    .catch(err => console.log(err));
-}
+    axios
+        .post(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${req.params.assignment_id}/submissions/update_grades`, qs.stringify(formData), headerData)
+        .then(result => {
+            res.send('success');
+        })
+        .catch(err => console.log(err));
+};
--- BackendV2/distribution-algorithm/grader-model.js
+++ BackendV2/distribution-algorithm/grader-model.js
@@ -7,88 +7,88 @@
  */
 
 function AssignmentGrader(grader_id, weight, offset, num_assigned) {
-  this.grader_id = grader_id;
-  this.weight = weight; //int
-  this.offset = offset; //int
-  this.num_assigned = num_assigned; //int 
+    this.grader_id = grader_id;
+    this.weight = weight; //int
+    this.offset = offset; //int
+    this.num_assigned = num_assigned; //int 
 }
 
 AssignmentGrader.prototype.updateWeight = function (weight) {
-  if (Number.isInteger(wieght)) {
-    throw new TypeError('wieght should be an integer')
-  }
-  this.weight = weight;
-}
+    if (Number.isInteger(wieght)) {
+        throw new TypeError('wieght should be an integer');
+    }
+    this.weight = weight;
+};
 
 AssignmentGrader.prototype.updateOffset = function (offset) {
-  if (!Number.isInteger(offset)) {
-    throw new TypeError('offset should be an integer')
-  }
+    if (!Number.isInteger(offset)) {
+        throw new TypeError('offset should be an integer');
+    }
 
-  this.offset = offset;
-}
+    this.offset = offset;
+};
 
 AssignmentGrader.prototype.updateNumAssigned = function (num_assigned) {
-  if (!Number.isInteger(num_assigned)) {
-    throw new TypeError('num_assigned should be an integer')
-  }
+    if (!Number.isInteger(num_assigned)) {
+        throw new TypeError('num_assigned should be an integer');
+    }
 
-  this.num_assigned = num_assigned;
-}
+    this.num_assigned = num_assigned;
+};
 
 
 AssignmentGrader.prototype.decrementWeight = function (weight) {
-  if (Number.isInteger(wieght)) {
-    throw new TypeError('wieght should be an integer')
-  }
-  this.weight -= weight;
-}
+    if (Number.isInteger(wieght)) {
+        throw new TypeError('wieght should be an integer');
+    }
+    this.weight -= weight;
+};
 
 AssignmentGrader.prototype.incrementWeight = function (weight) {
-  if (Number.isInteger(wieght)) {
-    throw new TypeError('wieght should be an integer')
-  }
-  this.weight += weight;
-}
+    if (Number.isInteger(wieght)) {
+        throw new TypeError('wieght should be an integer');
+    }
+    this.weight += weight;
+};
 
 
 
 
 AssignmentGrader.prototype.decrementOffset = function (offset) {
-  if (!Number.isInteger(offset)) {
-    throw new TypeError('offset should be an integer')
-  }
+    if (!Number.isInteger(offset)) {
+        throw new TypeError('offset should be an integer');
+    }
 
-  this.offset -= offset;
-}
+    this.offset -= offset;
+};
 
 AssignmentGrader.prototype.incrementOffset = function (offset) {
-  if (!Number.isInteger(offset)) {
-    throw new TypeError('offset should be an integer')
-  }
+    if (!Number.isInteger(offset)) {
+        throw new TypeError('offset should be an integer');
+    }
 
-  this.offset += offset;
-}
+    this.offset += offset;
+};
 
 
 
 
 AssignmentGrader.prototype.decrementNumAssigned = function (num_assigned) {
-  if (!Number.isInteger(num_assigned)) {
-    throw new TypeError('num_assigned should be an integer')
-  }
+    if (!Number.isInteger(num_assigned)) {
+        throw new TypeError('num_assigned should be an integer');
+    }
 
-  this.num_assigned -= num_assigned;
-}
+    this.num_assigned -= num_assigned;
+};
 
 AssignmentGrader.prototype.incrementNumAssigned = function (num_assigned) {
-  if (!Number.isInteger(num_assigned)) {
-    throw new TypeError('num_assigned should be an integer')
-  }
+    if (!Number.isInteger(num_assigned)) {
+        throw new TypeError('num_assigned should be an integer');
+    }
 
-  this.num_assigned += num_assigned;
-}
+    this.num_assigned += num_assigned;
+};
 
 
 
-module.exports = AssignmentGrader
+module.exports = AssignmentGrader;
