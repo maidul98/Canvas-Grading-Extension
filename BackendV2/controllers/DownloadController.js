3e8e5f66d79685fe8cc071c50e66985a7489446e
Ansh Godha <anshgodha714@gmail.com>
2020-04-03 11:31:59 +0530

    download function cleaned up with promises

--- BackendV2/controllers/DownloadController.js
+++ BackendV2/controllers/DownloadController.js
@@ -15,7 +15,7 @@ const config = {
     },
 };
 
-async function downloadHelper(attachment, filestream, filepath) {
+function downloadHelper(attachment, filestream, filepath) {
     https.get(attachment.url, function (response) {
         response.pipe(filestream);
         filestream.on('finish', function () {
@@ -23,6 +23,7 @@ async function downloadHelper(attachment, filestream, filepath) {
             console.log('filestream closed')
         });
     }).on('error', function (err) {
+        console.log(err)
         fs.unlink(`${filepath}/${attachment.filename}`);
     });;
 }
@@ -37,64 +38,68 @@ async function downloadHelper(attachment, filestream, filepath) {
  * @param assignment_id 
  * the id of the assignment we are going to be downloading for
  */
-async function createDownloadSubmission(batchDownloadPath, user_ids, assignment_id) {
+function createDownloadSubmission(batchDownloadPath, user_ids, assignment_id) {
     //make parent folder
-    console.log('started method')
-    mkdirp(batchDownloadPath)
-        .then(parentPath => {
-            user_ids.map(user_id => {
-                //make folder where we are going to save the this users files 
-                mkdirp(`${parentPath}/${user_id}`)
-                    .then(filePathForUser => {
-                        //get the list of attachments for a user
-                        axios
-                            .get(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${assignment_id}/submissions/${user_id}`, config)
-                            .then(result => {
-                                //for each attachment download the attachment and save 
-                                if (result.data.attachments) {
-                                    result.data.attachments.map(async function (attachment) {
-                                        //create a writeable file stream
-                                        const fileStream = fs.createWriteStream(`${filePathForUser}/${attachment.filename}`);
-                                        await downloadHelper(attachment, fileStream, filePathForUser);
-                                    })
-                                }
-                            })
-                            .catch(error => {
-                                return false
-                            })
-                    })
-                    .catch(error => {
-                        return false
-                    })
+    return new Promise((resolve, reject) => {
+        console.log('started method')
+        mkdirp(batchDownloadPath)
+            .then(parentPath => {
+                user_ids.map(user_id => {
+                    //make folder where we are going to save the this users files 
+                    mkdirp(`${parentPath}/${user_id}`)
+                        .then(filePathForUser => {
+                            //get the list of attachments for a user
+                            axios
+                                .get(`https://canvas.cornell.edu/api/v1/courses/15037/assignments/${assignment_id}/submissions/${user_id}`, config)
+                                .then(result => {
+                                    //for each attachment download the attachment and save 
+                                    if (result.data.attachments) {
+                                        result.data.attachments.map(async function (attachment) {
+                                            //create a writeable file stream
+                                            const fileStream = fs.createWriteStream(`${filePathForUser}/${attachment.filename}`);
+                                            await downloadHelper(attachment, fileStream, filePathForUser);
+                                        })
+                                    }
+                                })
+                                .catch(error => {
+                                    console.log(error);
+                                    reject(error)
+                                })
+                        })
+                        .catch(error => {
+                            console.log(error);
+                            reject(error)
+                        })
+                })
             })
-        })
-        .then(_ => {
-            return true
-        })
-        .catch(error => {
-            return false
-        })
+            .then(_ => {
+                resolve(true)
+            })
+            .catch(error => {
+                console.log(error);
+                reject(error)
+            })
+    })
+
 }
-
-
-
 
 exports.downloadSubmissions = function (req, res) {
     let batchDownloadPath = `temp_bulk_downloads/assignemnt-${req.body.assignment_id}-${req.body.grader_id}`;
-    console.log(batchDownloadPath)
 
     if (fs.existsSync(batchDownloadPath) && fs.lstatSync(batchDownloadPath).isDirectory()) {
-        console.log('in first if branch')
-        fs.remove(batchDownloadPath).then(() => {
-            if (createDownloadSubmission(batchDownloadPath, req.body.user_ids, req.body.assignment_id)) {
-                res.send('done downloading')
-            }
+        fs.remove(batchDownloadPath).then(function () {
+            // if (createDownloadSubmission(batchDownloadPath, req.body.user_ids, req.body.assignment_id)) {
+            //     res.send('done downloading')
+            // }
+            createDownloadSubmission(batchDownloadPath, req.body.user_ids, req.body.assignment_id)
+                .then(_ => res.send('done downloading'))
+                .catch(err => console.log(err))
 
         })
     } else {
-        if (createDownloadSubmission(batchDownloadPath, req.body.user_ids, req.body.assignment_id)) {
-            res.send('done downloading')
-        }
+        createDownloadSubmission(batchDownloadPath, req.body.user_ids, req.body.assignment_id)
+            .then(_ => res.send('done downloading'))
+            .catch(err => console.log(err))
     }
 }
 
