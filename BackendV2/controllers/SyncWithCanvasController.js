252df22175612cc33c7991d00a9779144fc5e0fe
Divya Talesra <dtstarartist@gmail.com>
2020-05-13 17:32:24 -0400

    making changes in controllers

--- BackendV2/controllers/SyncWithCanvasController.js
+++ BackendV2/controllers/SyncWithCanvasController.js
@@ -5,42 +5,52 @@ const submissions = require('../models/Submission')
 const axios = require('axios');
 
 const config = {
-    headers: {
-      Authorization: 'Bearer 9713~8gLsbC5WwTWOwqv8U3RPK4KK0wcgFThoufCz7fsCwXKsM00w9jKRcqFsbAo8HvJJ',
-      'Accept': 'application/json',
-    },
-  };
+  headers: {
+    Authorization: 'Bearer 9713~8gLsbC5WwTWOwqv8U3RPK4KK0wcgFThoufCz7fsCwXKsM00w9jKRcqFsbAo8HvJJ',
+    'Accept': 'application/json',
+  },
+};
 
 module.exports.syncWithCanvas = async (req, res) => {
-    try{
-        //get the list of assigments 
-          let assignments = await axios.get(`https://canvas.cornell.edu/api/v1/courses/15037/assignments?per_page=200`, config);
-          assignments = assignments.data.filter(assignment =>{
-            return assignment.workflow_state == "published";
-          });
+  try {
+    //get the list of assigments from Canvas
+    let assignments = await axios.get(`https://canvas.cornell.edu/api/v1/courses/15037/assignments?per_page=200`, config);
+    assignments = assignments.data.filter(assignment => {
+      return assignment.workflow_state === "published";
+    });
 
-          assignments = assignments.map(assignment =>{
-              return [assignment.id, assignment.name]
-          });
+    assignments = assignments.map(assignment => {
+      return [assignment.id, assignment.name]
+    });
 
-          assignment_ids = assignments.map(assignment =>{
-              return assignment[0]
-          })
+    assignment_ids = assignments.map(assignment => {
+      return assignment[0]
+    })
 
-        //populate assignment caps table with graders
-        let graders = await grader.getAll();
-        graders = graders.map(grader => grader.id);
-        await assignmentsCap.insertForAssignmentsForUsers(assignment_ids, graders);
+    //get the list of all graders from Canvas 
+    let list_of_graders = await axios.get(`https://canvas.cornell.edu/api/v1/courses/15037/enrollments?per_page=1000`, config);
+    list_of_graders = list_of_graders.data.filter(enrollment => {
+      return enrollment.type === "TeacherEnrollment";
+    });
 
-        //pull submissions for each assignment
-        for(let i = 0; i< assignment_ids.length; i++){
-            await submissions.pull_submissions_from_canvas(assignment_ids[i])
-        }
+    //get user ID and insert into the DB table -- graders 
+    await grader.insertGraders(list_of_graders);
 
-        //check if names of assigments updated
-        res.send();
-    }catch(error){
-        console.log(error)
-        res.status(406).send(error)
+
+    //populate assignment caps table with graders
+    let graders = await grader.getAll();
+    graders = graders.map(grader => grader.id);
+    await assignmentsCap.insertForAssignmentsForUsers(assignment_ids, graders);
+
+    //pull submissions for each assignment
+    for (let i = 0; i < assignment_ids.length; i++) {
+      await submissions.pull_submissions_from_canvas(assignment_ids[i])
     }
+
+    //check if names of assigments updated
+    res.send();
+  } catch (error) {
+    console.log(error)
+    res.status(406).send(error)
+  }
 }
--- BackendV2/models/Grader.js
+++ BackendV2/models/Grader.js
@@ -9,59 +9,59 @@ const axios = require('axios');
  * @param {*} res 
  */
 module.exports.grader_info = function (assignment_id) {
-    return new Promise(function(resolve, reject){
-        let sql_query = "SELECT grader.name, grader.id, assignments_cap.cap, grader.offset, assignments_cap.total_assigned_for_assignment, grader.weight FROM grader INNER JOIN assignments_cap ON grader.id=assignments_cap.grader_id WHERE assignments_cap.assignment_id = ?";
-        pool.query(sql_query, [assignment_id], async (error, results) => {
-          if (error) {
-            return reject(error)
-          } else {
-            let graderResponse = []
-            for (let i = 0; i < results.length; i++) {
-              let grader = results[i];
-              let tempGraderObj = grader;
-              let progressObj = await getNumGradedRatio(grader.id, assignment_id);
-              tempGraderObj['progress'] = progressObj.ratio;
-              tempGraderObj['num_graded'] = progressObj.num_graded;
-              tempGraderObj['assignment_id'] = assignment_id;
-              graderResponse.push(tempGraderObj);
-            }
-            resolve(graderResponse);
-          }
+  return new Promise(function (resolve, reject) {
+    let sql_query = "SELECT grader.name, grader.id, assignments_cap.cap, grader.offset, assignments_cap.total_assigned_for_assignment, grader.weight FROM grader INNER JOIN assignments_cap ON grader.id=assignments_cap.grader_id WHERE assignments_cap.assignment_id = ?";
+    pool.query(sql_query, [assignment_id], async (error, results) => {
+      if (error) {
+        return reject(error)
+      } else {
+        let graderResponse = []
+        for (let i = 0; i < results.length; i++) {
+          let grader = results[i];
+          let tempGraderObj = grader;
+          let progressObj = await getNumGradedRatio(grader.id, assignment_id);
+          tempGraderObj['progress'] = progressObj.ratio;
+          tempGraderObj['num_graded'] = progressObj.num_graded;
+          tempGraderObj['assignment_id'] = assignment_id;
+          graderResponse.push(tempGraderObj);
         }
-        );
-    })
+        resolve(graderResponse);
+      }
+    }
+    );
+  })
 }
 
 
 module.exports.updateGraderInfo = function (grader_object) {
-    return new Promise(async function(resolve, reject){
-      try{
-        const promisePool = pool.promise();
-        for(let i = 0; i<grader_object.length; i++){
-          if(grader_object[i].weight != undefined){
-            let sql_query = "UPDATE grader SET weight = ? WHERE id = ?";
-            await promisePool.query(sql_query, [grader_object[i].weight, grader_object[i].id]);
-          }
-          if(grader_object[i].offset != undefined){
-            let sql_query = "UPDATE grader SET offset = ? WHERE id = ?";
-            await promisePool.query(sql_query, [grader_object[i].offset, grader_object[i].id]);
-          }
-          if(grader_object[i].cap != undefined){
-            let sql_query = "UPDATE assignments_cap SET cap=? WHERE grader_id=? AND assignment_id=?";
-            await promisePool.query(sql_query, [grader_object[i].cap, grader_object[i].id, grader_object[i].assignment_id]);
-          }
+  return new Promise(async function (resolve, reject) {
+    try {
+      const promisePool = pool.promise();
+      for (let i = 0; i < grader_object.length; i++) {
+        if (grader_object[i].weight != undefined) {
+          let sql_query = "UPDATE grader SET weight = ? WHERE id = ?";
+          await promisePool.query(sql_query, [grader_object[i].weight, grader_object[i].id]);
         }
-        return resolve();
-      }catch(error){
-        reject(error)
+        if (grader_object[i].offset != undefined) {
+          let sql_query = "UPDATE grader SET offset = ? WHERE id = ?";
+          await promisePool.query(sql_query, [grader_object[i].offset, grader_object[i].id]);
+        }
+        if (grader_object[i].cap != undefined) {
+          let sql_query = "UPDATE assignments_cap SET cap=? WHERE grader_id=? AND assignment_id=?";
+          await promisePool.query(sql_query, [grader_object[i].cap, grader_object[i].id, grader_object[i].assignment_id]);
+        }
       }
+      return resolve();
+    } catch (error) {
+      reject(error)
+    }
   })
 }
 
 /**
  * Gets all of the users in the graders table 
  */
-module.exports.getAll = async function() {
+module.exports.getAll = async function () {
   return new Promise((resolve, reject) => {
     let query = "SELECT * FROM grader";
     pool.query(query, (err, results) => {
@@ -79,21 +79,21 @@ module.exports.getAll = async function() {
  * @param {*} user_id 
  * @param {*} assignment_id 
  */
-getNumGradedRatio = function (grader_id, assignment_id){
+getNumGradedRatio = function (grader_id, assignment_id) {
   return new Promise((resolve, reject) => {
     let query = "SELECT * FROM submission WHERE assignment_id =? AND grader_id = ?";
     pool.query(query, [assignment_id, grader_id], (err, results) => {
       if (err) {
         return reject(err)
       } else {
-        if(results.length == null | results.length == undefined | results.length == 0){return resolve(0)}
+        if (results.length == null | results.length == undefined | results.length == 0) { return resolve(0) }
         let total_gradered = 0
-        results.map(submission =>{
-          if(submission.is_graded == 1){
-            total_gradered+=1
+        results.map(submission => {
+          if (submission.is_graded == 1) {
+            total_gradered += 1
           }
         })
-        let progressObj =  {"num_graded": total_gradered,"ratio": (total_gradered/results.length)*100}
+        let progressObj = { "num_graded": total_gradered, "ratio": (total_gradered / results.length) * 100 }
         resolve(progressObj)
       }
     });
@@ -103,15 +103,15 @@ getNumGradedRatio = function (grader_id, assignment_id){
 /**
  * Returns a boolean on whether user with email exists in DB
  */
-module.exports.userWithEmailExists = async function (email){
-  try{
+module.exports.userWithEmailExists = async function (email) {
+  try {
     let promisePool = pool.promise();
-    const [user,fields] = await promisePool.query("SELECT * from grader where email=?", [email])
-    if(user ==[] | user == undefined | user.length == 0){
+    const [user, fields] = await promisePool.query("SELECT * from grader where email=?", [email])
+    if (user == [] | user == undefined | user.length == 0) {
       return false
     }
     return user[0]
-  }catch(error){
+  } catch (error) {
     throw error
   }
 }
@@ -120,12 +120,12 @@ module.exports.userWithEmailExists = async function (email){
 /*
 Give the users bear token for Canvas requests.
  */
-module.exports.getCanvasReqConfig = async function (userId){
-  try{
+module.exports.getCanvasReqConfig = async function (userId) {
+  try {
     let promisePool = pool.promise();
-    const [bearToken,fields] = await promisePool.query("SELECT c_token from grader where id=?", [userId]);
+    const [bearToken, fields] = await promisePool.query("SELECT c_token from grader where id=?", [userId]);
 
-    if(bearToken == []){
+    if (bearToken == []) {
       throw new Error("Please add a Canvas Token");
     }
     var decryptedToken = encryptor.decrypt(bearToken[0].c_token);
@@ -139,7 +139,7 @@ module.exports.getCanvasReqConfig = async function (userId){
 
     return configConstruct;
 
-  }catch(error){
+  } catch (error) {
     throw error
   }
 }
@@ -148,8 +148,8 @@ module.exports.getCanvasReqConfig = async function (userId){
 /**
  * Validates and updates token
  */
-module.exports.updateCanvasToken = async function (user_id, token){
-  try{
+module.exports.updateCanvasToken = async function (user_id, token) {
+  try {
     let promisePool = pool.promise();
     let canvasReqConfig = {
       headers: {
@@ -164,7 +164,7 @@ module.exports.updateCanvasToken = async function (user_id, token){
     //encryptor
     var encryptoredToken = encryptor.encrypt(token);
     await promisePool.query("UPDATE grader SET c_token =? WHERE id=?", [encryptoredToken, user_id])
-  }catch(error){
+  } catch (error) {
     throw new Error('Your canvas token may have expired, please try adding a new token and try again')
   }
 }
