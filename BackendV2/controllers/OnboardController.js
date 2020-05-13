ce5af0dc54390b60271620f877e8a3b450278c2e
Maidul Islam <maidul98@gmail.com>
2020-05-13 14:52:03 -0400

    updaded downlaods so that token is pulled from db for Canvas request

--- BackendV2/controllers/OnboardController.js
+++ BackendV2/controllers/OnboardController.js
@@ -5,9 +5,8 @@ const grader = require('../models/Grader')
 module.exports.addCanvasToken = async (req, res) => {
     try{
         grader_array = await grader.updateCanvasToken(req.user.id, req.body.token)
-        res.send("Token as been updated")
+        res.send("Token as been encrypted and saved")
     }catch(error){
-        console.log(error)
         res.status(500).send(error.message)
     }
 }
