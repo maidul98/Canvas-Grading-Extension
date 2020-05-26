f6c8ad83905c56198e6077b07ff6d04867da28da
Maidul Islam <maidul98@gmail.com>
2020-05-26 18:29:58 -0400

    fixed url

---
+++ BackendV2/public/precache-manifest.c36542c22eb384c2ce6b17007c4c8909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8309c58208f6bbec1a98e22b908dca5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d710f9a67ef78e0b5e74",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "424a1900a53badf55c70",
+    "url": "/static/css/main.e03cc7b8.chunk.css"
+  },
+  {
+    "revision": "d710f9a67ef78e0b5e74",
+    "url": "/static/js/2.ea0a7cc1.chunk.js"
+  },
+  {
+    "revision": "cb6a4b5dabe3dddfa89f8414b4b6d1f4",
+    "url": "/static/js/2.ea0a7cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424a1900a53badf55c70",
+    "url": "/static/js/main.5ff0be4d.chunk.js"
+  },
+  {
+    "revision": "245ab64ca50bd93f9260",
+    "url": "/static/js/runtime-main.c3545834.js"
+  }
+]);
